07581e8348afc73ee2bd4e302680bef341cb43b6
Narendra Yogi <127980699+narendrayogi914@users.noreply.github.com>
2024-02-05 06:41:44 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server listening on *:3000');
+});
